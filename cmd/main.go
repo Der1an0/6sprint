eef8bc9f2189f4231c5e3f805990c43ba0add99b
Андрей Блохин <Der1ano@yandex.ru>
2025-10-31 12:55:51 +0300

    first

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	// Создаем логгер
+	logger := log.New(os.Stdout, "MORSE_SERVICE: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Создаем сервер
+	srv := server.New(logger)
+
+	// Запускаем сервер
+	if err := srv.Start(); err != nil {
+		logger.Fatalf("Server startup error: %v", err)
+	}
 }
